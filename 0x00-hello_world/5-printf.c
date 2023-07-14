e0e66f3754df31c50a1c891a8c3474b3dd6c9e81
Victor Aloba <alobavb@gmail.com>
2023-07-13 17:27:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - using the printf function
+*
+* Return:0 (Valid)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
