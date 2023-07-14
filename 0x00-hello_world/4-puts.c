9acab358be3e63fad5722f128d0f07e8f21ee7aa
Victor Aloba <alobavb@gmail.com>
2023-07-13 17:14:39 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A Program that prints with the function puts
+ *
+ * Return: 0 (Valid)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
